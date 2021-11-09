de02103f8c69f5a02b65c4001c9baab635ba33de
Leonildo Muniz <leonildomuniz20@gmail.com>
2021-11-08 22:32:28 -0300

    Teste de formatação

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,27 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Column(children: <Widget> [],),
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+              "texto 1",
+            style: TextStyle(
+              fontSize: 35,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0,
+              wordSpacing: 0,
+              decoration: TextDecoration.none,
+              decorationColor: Colors.black,
+              decorationStyle: TextDecorationStyle.solid,
+              color: Colors.black
+            ),
+          )
+        ],
+      ) ,
+    ),
+
   ));
+}
